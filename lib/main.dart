b978826f286ae10ee2cffab50e898dbf267868bd
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-18 14:57:57 +0500

    project setup is completed andd assets is added and splash view is created

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:solar_system/view/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: SplashView()
+    );
+  }
+}
